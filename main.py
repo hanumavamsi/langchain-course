254813f72024830c3e2846bd86dd26a542b8312f
hanumavamsi <hanumavamsi2001@gmail.com>
2026-01-20 03:05:23 +0530

    Learnt about structured output

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from langchain_tavily import TavilySearch
 from prompt import REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS
 from langchain_core.prompts import PromptTemplate
 from langchain_core.runnables import RunnableLambda
-from langchain_core.output_parsers import PydanticOutputParser
+# from langchain_core.output_parsers import PydanticOutputParser
 from schemas import AgentOutput
 
 
@@ -19,14 +19,20 @@ tools = [TavilySearch()]
 
 # The reason we are using gpt-4 and not gpt-5 is that as of June 2024, gpt-5 does not support function calling which is required for the React agent to work properly
 llm = ChatOpenAI(model="gpt-4", temperature=0)
+
+# structured output parsing using pydantic models
+# Instead of the formatting the output ourselves, we can leverage the pydantic model to define the structure of the output we want from the LLM
+# Difference between structured_llm and pydantic output parser is that structured_llm directly integrates with the LLM to produce structured output
+structured_llm = llm.with_structured_output(AgentOutput)
+
 react_prompt = hub.pull("hwchase17/react")
 
 # Define the output parser using the Pydantic model -> this will ensure the agent's output adheres to the schema
-output_parser = PydanticOutputParser(pydantic_object=AgentOutput)
+# output_parser = PydanticOutputParser(pydantic_object=AgentOutput)
 
 # Create a PromptTemplate from the REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS
 react_prompt_template = PromptTemplate(template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS, input_variables=["input", "tools", "tool_names", "agent_scratchpad", "format_instructions"]).partial(
-    format_instructions = output_parser.get_format_instructions()
+    format_instructions = ""
 )
 
 # Create the React agent with the custom prompt and output parser -> The output parser will validate the output using the schema (pydantic model)
@@ -41,12 +47,12 @@ extract_output = RunnableLambda(
 )
 
 # Runnable to parse the output using the Pydantic output parser
-parse_output = RunnableLambda(
-    lambda x: output_parser.parse(x)
-)
+# parse_output = RunnableLambda(
+#     lambda x: output_parser.parse(x)
+# )
 
 # Compose the runnables into a chain
-chain = agent_executor | extract_output | parse_output
+chain = agent_executor | extract_output | structured_llm
 
 
 def main():
